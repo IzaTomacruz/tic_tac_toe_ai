6f7ffb7a6c7079cd280347b972cfa9bc84e3eab2
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-06-24 17:42:00 +0800

    added fuction for drawing the cross and circle in the board

--- constants.py
+++ constants.py
@@ -9,7 +9,14 @@ class Constants:
 
         self.square_size = self.width // self.columns
         self.line_width = 15
+        self.circle_width = 15
+        self.cross_width = 20
+
+        self.radius = self.square_size // 4
+        self.offset = 50
 
         #Colors
         self.background = (255, 51, 153)
         self.line_color = (204, 0, 102)
+        self.circle_color = (102, 255, 255)
+        self.cross_color = (255, 0, 0)
--- tictactoe.py
+++ tictactoe.py
@@ -30,6 +30,7 @@ class FullGame(Constants):
                     if board.empty_squares(row, column):
                         board.mark_squares(row, column, self.game.player)
                         self.game.next_turn()
+                        self.game.draw_figure(row, column)
                         print(board.squares)
 
             pygame.display.update()
@@ -55,14 +56,37 @@ class Game(Constants):
 
     def show_lines(self):
         #For vertical
-        pygame.draw.line(self.screen, self.line_color, (self.square_size, 0), (self.square_size, self.height), self.line_width)
-        pygame.draw.line(self.screen, self.line_color, (self.width - self.square_size, 0), (self.width - self.square_size, self.height), self.line_width)
+        pygame.draw.line(self.screen, self.line_color, (self.square_size, 0),
+                         (self.square_size, self.height), self.line_width)
+        pygame.draw.line(self.screen, self.line_color, (self.width - self.square_size, 0),
+                         (self.width - self.square_size, self.height), self.line_width)
         #For horizontal
-        pygame.draw.line(self.screen, self.line_color, (0, self.square_size), (self.width, self.square_size), self.line_width)
-        pygame.draw.line(self.screen, self.line_color, (0, self.height - self.square_size), (self.width, self.height - self.square_size), self.line_width)
+        pygame.draw.line(self.screen, self.line_color, (0, self.square_size),
+                         (self.width, self.square_size), self.line_width)
+        pygame.draw.line(self.screen, self.line_color, (0, self.height - self.square_size),
+                         (self.width, self.height - self.square_size), self.line_width)
+
+    def draw_figure(self, row, column):
+        if self.player == 1:
+            #ascending
+            start_asc = (column * self.square_size + self.offset,
+                         row * self.square_size + self.square_size - self.offset)
+            end_asc = (column * self.square_size + self.square_size - self.offset,
+                       row * self.square_size + self.offset)
+            pygame.draw.line(self.screen, self.cross_color, start_asc, end_asc, self.cross_width)
+
+            #descending
+            start_desc = (column * self.square_size + self.offset,
+                          row * self.square_size + self.offset)
+            end_desc = (column * self.square_size + self.square_size - self.offset,
+                        row * self.square_size + self.square_size - self.offset)
+            pygame.draw.line(self.screen, self.cross_color, start_desc, end_desc, self.cross_width)
+
+        elif self.player == 2:
+            center = (column * self.square_size + self.square_size // 2, row * self.square_size + self.square_size // 2)
+            pygame.draw.circle(self.screen, self.circle_color, center, self.radius, self.circle_width)
 
     def next_turn(self):
         self.player = self.player % 2 + 1
 
-game_instance = FullGame()
-game_instance.loop()
+
